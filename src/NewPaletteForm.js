8a3bddc301bcd4f601173bb65f5f02b554111508
koftov <ahronlu@gmail.com>
2020-06-09 14:36:06 +0300

    Add ChromePicker and Buttons to drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -73,16 +73,9 @@ const styles = (theme) => ({
 });
 
 class NewPaletteForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: true,
-      currentColor: 'teal',
-      colors: ['purple', '#e15764'],
-    };
-    this.updateCurrentColor = this.updateCurrentColor.bind(this);
-    this.addNewColor = this.addNewColor.bind(this);
-  }
+  state = {
+    open: false,
+  };
 
   handleDrawerOpen = () => {
     this.setState({ open: true });
@@ -91,14 +84,6 @@ class NewPaletteForm extends Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
-
-  updateCurrentColor(newColor) {
-    this.setState({ currentColor: newColor.hex });
-  }
-
-  addNewColor() {
-    this.setState({ colors: [...this.state.colors, this.state.currentColor] });
-  }
 
   render() {
     const { classes } = this.props;
@@ -152,15 +137,10 @@ class NewPaletteForm extends Component {
             </Button>
           </div>
           <ChromePicker
-            color={this.state.currentColor}
-            onChangeComplete={this.updateCurrentColor}
+            color="purple"
+            onChangeComplete={(newColor) => console.log(newColor)}
           />
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ backgroundColor: this.state.currentColor }}
-            onClick={this.addNewColor}
-          >
+          <Button variant="contained" color="primary">
             Add Color
           </Button>
         </Drawer>
@@ -170,11 +150,6 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          <ul>
-            {this.state.colors.map((color) => (
-              <li style={{ backgroundColor: color }}>{color}</li>
-            ))}
-          </ul>
         </main>
       </div>
     );
